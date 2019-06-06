88d277bb32569e93c878db499b43c51cdd0004b7
koreancoco <koreancoco@163.com>
2019-06-06 23:07:02 +0800

    @加入util2

---
+++ src/com/demo/utils/Util2.java
@@ -0,0 +1,10 @@
+package com.demo.utils;/**
+  *ClassNmae:  Util2
+  *Description:  TODO
+  *@author  MasonWu
+  *@date  2019/6/6 23:06
+  *@version  1.0
+  *Copyright (c) 2018-2020 Koreancoco All Rights Reserved.
+  **/
+public class Util2 {
+}
